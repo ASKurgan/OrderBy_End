610d3efd94c6c09d76439cae0760537eb5d27dd7
ASKurgan <infopret@yandex.ru>
2023-07-12 18:47:22 +0300

    first

---
+++ OrderByList_1_3/Helper.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+
+public class Helper
+{
+    public static bool IsDigit(string str)
+    {
+        if (String.IsNullOrEmpty(str))
+        {
+            Console.WriteLine("Вы ввели пустую строку");
+            return false;
+        }
+        string[] strArray = new string[str.Length];
+        bool tFValue = true, isFirstComma = true;
+        for (int i = 0; i < str.Length; i++)
+        {
+            strArray[i] = str.Substring(i, 1);
+            if(i == 0 && strArray[i] == "-")
+            {
+               continue;
+            }
+            if(isFirstComma && i > 0 && strArray[i-1] != "-" && strArray[i] == ",")
+            {
+                isFirstComma = false;
+                continue;
+
+            }
+            switch (strArray[i])
+            {
+                case "0":
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+
+                default:
+                    System.Console.WriteLine("Вы ввели неверное значение, попробуйте заново");
+                    tFValue = false;
+                    break;
+            }
+
+
+            if (!tFValue)
+            {
+                break;
+            }
+        }
+        return tFValue;
+    }
+
+    public static string DeleteNegativeZero(string str)
+    {
+        Regex regex = new Regex("^-0$");
+        MatchCollection matches = regex.Matches(str);
+        if (matches.Count > 0)
+        {
+            return "0";
+        }
+        else
+        {
+            return str;
+        }
+    }
+}
---
+++ OrderByList_1_3/Program.cs
@@ -0,0 +1,84 @@
+﻿// Коллекция, лист. Вывод на консоль упорядоченного массива
+
+using System.Text.RegularExpressions;
+
+System.Console.WriteLine("Введите числа:");
+System.Console.WriteLine();
+var myList = new List<double>();
+bool tF = true;
+
+int sch = 0;
+string? strValue,str, strValueContinue, strValueContinueOne;
+double value;
+while (tF)
+{
+    
+    //++sch;
+    System.Console.WriteLine($"Введите {sch + 1}-е число: ");
+
+    strValue = System.Console.ReadLine();
+    str = (strValue == null) ? string.Empty : strValue;
+        
+    if (Helper.IsDigit(str))
+    {
+        value = double.Parse(str);
+        myList.Add(value);
+        System.Console.WriteLine();
+        foreach (var item in myList)
+        {
+            System.Console.Write($"{Helper.DeleteNegativeZero(item.ToString())}   ");
+        }
+        System.Console.WriteLine();
+
+        ++sch;
+
+
+        if (sch > 1)
+        {
+
+
+            System.Console.WriteLine("Хотите продолжить заполнение списка или отсортировать? Любой символ - продолжить , s - отсортировать список");
+            strValueContinueOne = Console.ReadLine();
+            strValueContinue = (strValueContinueOne == null) ? string.Empty : strValueContinueOne;
+            if (strValueContinue.ToLower() == "s")
+            {
+                tF = false;
+            }
+        }
+    }
+}
+
+System.Console.WriteLine("Неотсортированный список:");
+System.Console.WriteLine();
+
+var countArray = myList.Count;
+double[] myArray = new double [countArray]; 
+for (var i = 0; i< myList.Count; i++)
+{
+    myArray[i] = myList[i]; 
+    System.Console.Write(myList[i].ToString() + "   ");
+}
+System.Console.WriteLine();
+
+
+System.Console.WriteLine();
+for (int i = 0; i < countArray; i++)
+{
+    for ( int j = 0; j < countArray - i - 1; j++ )
+    {
+       var interValue =  myArray[j];
+       if(interValue > myArray[j+1] )
+       {
+        myArray[j] = myArray[j+1];
+        myArray[j+1] = interValue;
+       }   
+    }
+}
+
+System.Console.WriteLine("Отсортированный список:");
+System.Console.WriteLine();
+for (int i = 0; i < countArray; i++)
+{
+    System.Console.Write(Helper.DeleteNegativeZero(myArray[i].ToString())  + "   " );
+}
+System.Console.ReadLine();
